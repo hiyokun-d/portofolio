3961760e135c046721aaaf363ca72ed13749643d
nothing <ddwi2504@gmail.com>
2025-08-21 15:27:12 +0800

    fixed our stupid mistake in about me section

--- src/components/react/GridGen.jsx
+++ src/components/react/GridGen.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useMemo, useCallback } from 'react';
 import { animate, onScroll, stagger } from 'animejs';
+import { isMobileDevice } from '../../function/isMobileDevice';
 
-const GRID_WIDTH = 10;
-const GRID_HEIGHT = 6;
+export const GRID_WIDTH = 10;
+export const GRID_HEIGHT = 6;
 
 export default function GridGen({ }) {
     let animating = false
     const handleClick = useCallback((e) => {
         const fromIndex = parseInt(e.currentTarget.dataset.index, 10);
-        if (animating) return;
+        if (animating && isMobileDevice) return;
         animating = true;
 
         animate(".bg-cell", {
@@ -31,42 +32,16 @@ export default function GridGen({ }) {
         });
     }, []);
 
-    useEffect(() => {
-        animate(".bg-cell", {
-            border: ["50px solid red", "1px solid rgba(96, 96, 96, 0.1)"],
-            backgroundColor: ["rgba(255, 255, 255, 0.1)", "rgba(255, 0, 0, 0.5)", "rgba(255, 255, 255, 0)"],
-            delay: stagger(65, { grid: [GRID_WIDTH, GRID_HEIGHT], from: "center" }),
-            duration: 450,
-            easing: 'inBounce',
-            autoplay: onScroll({
-
-                enter: "enter+=80 bottom-=80",
-                onLeaveForward: () => {
-                    animate(".bg-cell", {
-                        backgroundColor: "rgba(255, 255, 255, 0.1)",
-                        border: "1px solid rgba(40, 40, 40, 0.333)",
-                        translateY: -10,
-                        boxShadow: "0px 2px 5px rgba(198, 198, 198, 0.5)"
-                    });
-                }
-            })
-        })
-
-    }, []);
-
     const rect = useMemo(() => {
-        const cells = [];
-        for (let i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++) {
-            cells.push(
-                <div
-                    key={i}
-                    className="bg-cell w-full h-full bg-transparent border-0"
-                    data-index={i}
-                    onClick={handleClick}
-                />
-            );
-        }
-        return cells;
+        const totalCells = 60; // 10 x 6 but still flexible
+        return Array.from({ length: totalCells }, (_, i) => (
+            <div
+                key={i}
+                className="bg-cell w-full h-full bg-transparent border-0"
+                data-index={i}
+                onClick={handleClick}
+            />
+        ));
     }, [handleClick]);
 
     return <>{rect}</>;
